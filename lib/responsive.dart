9797700c222bcf59227dc7c9f0e7b966b97ad6b6
Merna <es-Merna.William2023@alexu.edu.eg>
2022-12-15 14:43:23 +0200

    added responsive parameters

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget desktop;
+
+  const Responsive({
+    required Key key,
+    required this.mobile,
+    required this.desktop,
+  }) : super(key: key);
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1100;
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1100;
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).size.width < 1100) {
+      return mobile;
+    } else
+      return desktop;
+  }
+}
