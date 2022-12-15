910e340b961c0d1b033c8d02d9d963dec1265f67
amrmohamed25 <amrmohamedawed80@gmail.com>
2022-12-15 14:25:58 +0200

    added authentication to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ar_furniture_admin_panel/screens/login.dart';
+import 'package:ar_furniture_admin_panel/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
